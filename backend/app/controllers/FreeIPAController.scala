be2b5de658d23ea8089d99199b7f16acb1102d83
pruthjara <pruthjperez@gmail.com>
2024-12-14 18:52:21 +0100

    Actualizar controller y nuevos imports

--- backend/app/controllers/FreeIPAController.scala
+++ backend/app/controllers/FreeIPAController.scala
@@ -1,21 +1,9 @@
-// app/controllers/FreeIPAController.scala
 package controllers
 
-import javax.inject._
-import play.api.libs.json._
-import play.api.mvc._
+// Importación de librerías necesarias para el manejo de JSON, controladores y dependencias
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{AbstractController, ControllerComponents}
+import providers.FreeIPAProvider
 
-@Singleton
-class FreeIPAController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-
-  // Lista de usuarios ficticios simulando datos de FreeIPA
-  private val fakeUsers = Json.arr(
-    Json.obj("username" -> "hugopascual", "fullName" -> "Hugo Pascual", "email" -> "hugopascual@upm.es", "uid" -> 1001, "gid" -> 1001, "lastLogin" -> "2024-10-30T14:05:46Z", "status" -> "active"),
-    Json.obj("username" -> "pruthjara", "fullName" -> "Pilar Jara", "email" -> "ruth.jperez@alumnos.upm.es", "uid" -> 1002, "gid" -> 1001, "lastLogin" -> "2024-10-29T11:25:35Z", "status" -> "inactive")
-  )
-
-  // Método para devolver los usuarios ficticios en JSON
-  def getUsers: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(fakeUsers)
-  }
-}
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
