6fc4e7b4069d6e0cfc354bc703f532edcce99810
pruthjara <pruthjperez@gmail.com>
2024-12-14 18:54:16 +0100

    Definir controlador

--- backend/app/controllers/FreeIPAController.scala
+++ backend/app/controllers/FreeIPAController.scala
@@ -7,3 +7,12 @@ import providers.FreeIPAProvider
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
+
+
+// Definición de un controlador como un singleton, asegurando que haya solo una instancia en toda la aplicación
+@Singleton
+class FreeIPAController @Inject() (
+    cc: ControllerComponents, // Inyección de los componentes del controlador (por ejemplo, acciones HTTP)
+    freeIPAProvider: FreeIPAProvider // Inyección del proveedor de servicios FreeIPA
+)(implicit ec: ExecutionContext) // Contexto de ejecución para manejar operaciones asíncronas
+    extends AbstractController(cc) {
