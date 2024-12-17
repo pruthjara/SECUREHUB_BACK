a93261bfaa11449714e5ed7b8a9c53910dc4b829
pruthjara <ruth.jperez@alumnos.upm.es>
2024-12-17 22:33:36 +0100

    Añadir controller keycloak

--- backend/app/controllers/SecureController.scala
+++ backend/app/controllers/SecureController.scala
@@ -1,19 +1,17 @@
 package controllers
 
-import org.pac4j.play.scala.SecurityAction
+import org.pac4j.play.scala.{SecureAction, SecureActionBuilder}
 import play.api.mvc._
 import javax.inject.Inject
 import security.SecurityConfig
 
 class SecureController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  // Crea una instancia de SecurityAction
-  private val securityAction = new SecurityAction[play.api.mvc.AnyContent](
-    clients = SecurityConfig.buildClients()
-  )
+  // Instancia de SecureAction
+  private val secureAction = new SecureActionBuilder(SecurityConfig.buildClients())
 
   // Endpoint protegido
-  def secureEndpoint: Action[AnyContent] = securityAction { request =>
+  def secureEndpoint: Action[AnyContent] = secureAction { request =>
     Ok("¡Acceso autorizado! Has iniciado sesión correctamente.")
   }
 
