81038f2ae435641c9e20a239ef508cd18299b854
pruthjara <124813402+pruthjara@users.noreply.github.com>
2025-05-23 16:00:22 +0200

    Update FreeIPAProvider.scala

--- backend/app/providers/FreeIPAProvider.scala
+++ backend/app/providers/FreeIPAProvider.scala
@@ -1,43 +1,35 @@
 package providers
 
-// Importación de librerías necesarias para la inyección de dependencias, manejo de JSON y registro de logs
 import javax.inject.{Inject, Singleton}
 import play.api.libs.json.{JsValue, Json}
 import play.api.Logging
 import scala.concurrent.{ExecutionContext, Future}
 import scala.sys.process._
 
-
-// Definición de un proveedor como un singleton para manejar servicios relacionados con FreeIPA
 @Singleton
 class FreeIPAProvider @Inject()(implicit ec: ExecutionContext) extends Logging {
-  // URL base para las solicitudes a FreeIPA
   private val freeIPAUrl = "https://freeipa.andion.eu/ipa/session/json"
 
-  // Método privado para ejecutar un comando curl dado un payload
   private def executeCurl(payload: String): JsValue = {
-    // Construye el comando curl como una secuencia
     val curlCommand = Seq(
       "curl",
-      "--negotiate", // Autenticación Kerberos
-      "-u", ":", // Usuario vacío para Kerberos
-      "-k", // Ignora errores de certificado SSL
-      "-X", "POST", // Método POST
-      "-H", "Content-Type: application/json", // Cabecera para tipo de contenido
-      "-H", s"Referer: $freeIPAUrl", // Referer para la solicitud
-      "-d", payload, // Cuerpo de la solicitud
-      freeIPAUrl // URL de destino
+      "--negotiate", 
+      "-u", ":", 
+      "-k", 
+      "-X", "POST", 
+      "-H", "Content-Type: application/json", 
+      "-H", s"Referer: $freeIPAUrl", 
+      "-d", payload, 
+      freeIPAUrl 
     )
 
-    logger.info(s"Executing curl command: ${curlCommand.mkString(" ")}") // Registra el comando curl ejecutado
-    val result = curlCommand.!! // Ejecuta el comando y obtiene el resultado
-    logger.info(s"Received response: $result") // Registra la respuesta obtenida
-    Json.parse(result) // Convierte la respuesta en un objeto JSON
+    logger.info(s"Executing curl command: ${curlCommand.mkString(" ")}") 
+    val result = curlCommand.!! 
+    logger.info(s"Received response: $result") 
+    Json.parse(result) 
   }
 
-  // Método para obtener los detalles de un usuario dado un nombre de usuario
   def getUser(username: String): Future[JsValue] = Future {
-    // Construye el payload JSON para la solicitud de detalles de usuario
     val payload =
       s"""{
          |  "method": "user_show",
@@ -45,12 +37,11 @@ class FreeIPAProvider @Inject()(implicit ec: ExecutionContext) extends Logging {
          |  "version": "2.254"
          |}""".stripMargin
 
-    executeCurl(payload) // Ejecuta el comando curl con el payload
+    executeCurl(payload)
   }
 
-  // Método para obtener todos los grupos disponibles
+ 
   def getGroups: Future[JsValue] = Future {
-    // Construye el payload JSON para la solicitud de grupos
     val payload =
       s"""{
          |  "method": "group_find",
@@ -58,12 +49,12 @@ class FreeIPAProvider @Inject()(implicit ec: ExecutionContext) extends Logging {
          |  "version": "2.254"
          |}""".stripMargin
 
-    executeCurl(payload) // Ejecuta el comando curl con el payload
+    executeCurl(payload) 
   }
 
-  // Método para obtener todos los usuarios disponibles
+
   def getAllUsers: Future[JsValue] = Future {
-    // Construye el payload JSON para la solicitud de usuarios
+   
     val payload =
       s"""{
          |  "method": "user_find",
@@ -71,12 +62,12 @@ class FreeIPAProvider @Inject()(implicit ec: ExecutionContext) extends Logging {
          |  "version": "2.254"
          |}""".stripMargin
 
-    executeCurl(payload) // Ejecuta el comando curl con el payload
+    executeCurl(payload)
   }
 
-  // Método para obtener los detalles de un grupo dado un nombre de grupo
+
   def getGroup(groupname: String): Future[JsValue] = Future {
-    // Construye el payload JSON para la solicitud de detalles del grupo
+ 
     val payload =
       s"""{
         |  "method": "group_show",
@@ -84,7 +75,7 @@ class FreeIPAProvider @Inject()(implicit ec: ExecutionContext) extends Logging {
         |  "version": "2.254"
         |}""".stripMargin
 
-    executeCurl(payload) // Ejecuta el comando curl con el payload
+    executeCurl(payload) 
   }
 
 }
